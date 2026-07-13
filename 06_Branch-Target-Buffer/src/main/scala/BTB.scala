fba3258e6a5488d92b439f60ca8550ed6bf03d9b
Umer-Farooq <umerfarooq311853@gmail.com>
2026-07-13 13:25:59 +0200

    remaining files

--- 06_Branch-Target-Buffer/src/main/scala/BTB.scala
+++ 06_Branch-Target-Buffer/src/main/scala/BTB.scala
@@ -17,7 +17,7 @@ class BTB extends Module {
     val predictTaken = Output(Bool())
   })
 
-  // << CHANGED in A06 >>
+
   //Why: Spec requires 8 sets, 2-way set-associative BTB
   val numSets   = 8
   val numWays   = 2
@@ -30,7 +30,7 @@ class BTB extends Module {
   val targetRegs = RegInit(VecInit(Seq.fill(numSets)(VecInit(Seq.fill(numWays)(0.U(32.W))))))
   val stateRegs  = RegInit(VecInit(Seq.fill(numSets)(VecInit(Seq.fill(numWays)(1.U(2.W))))))
 
-  // << NEW in A06 >>
+
   //Why: With 2 ways, 1 bit per set marks which way to evict next
   val lruWay = RegInit(VecInit(Seq.fill(numSets)(0.U(1.W))))
 
@@ -47,7 +47,7 @@ class BTB extends Module {
   io.target       := Mux(hit1, targetRegs(lookupIndex)(1), targetRegs(lookupIndex)(0))
   io.predictTaken := hit && Mux(hit1, stateRegs(lookupIndex)(1)(1), stateRegs(lookupIndex)(0)(1))
 
-  // << NEW in A06 >>
+
   //Why: A lookup hit counts as an access for LRU tracking
   when(hit0) {
     lruWay(lookupIndex) := 1.U
@@ -63,7 +63,7 @@ class BTB extends Module {
   val updHit1 = validRegs(updateIndex)(1) && (tagRegs(updateIndex)(1) === updateTag)
   val updHit  = updHit0 || updHit1
 
-  // << NEW in A06 >>
+
   //Why: On a miss we allocate an invalid way first, otherwise evict the LRU way
   val allocWay = Mux(!validRegs(updateIndex)(0), 0.U,
                   Mux(!validRegs(updateIndex)(1), 1.U, lruWay(updateIndex)))
@@ -72,7 +72,7 @@ class BTB extends Module {
 
   val oldState = stateRegs(updateIndex)(selectedWay)
 
-  // << NEW in A06 >>
+  
   //Why: Recover the real branch direction from the mispredicted signal
   val oldPredictTaken = updHit && oldState(1)
   val actualTaken     = Mux(updHit, oldPredictTaken =/= io.mispredicted, io.mispredicted)
